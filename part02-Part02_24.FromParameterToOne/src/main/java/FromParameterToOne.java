abfc60eb408839fe620444755cc4c0383b38dceb
Sparkimouto <155643846+HimoutoCalcifer@users.noreply.github.com>
2025-01-20 15:36:41 +0800

    Add files via upload

---
+++ part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
@@ -0,0 +1,21 @@
+
+
+public class FromParameterToOne {
+
+    public static void main(String[] args) {
+
+        printFromNumberToOne(2);
+
+
+    }
+
+    public static void printFromNumberToOne(int number) {
+
+
+        while (number > 0) {
+            System.out.println(number);
+            number--;
+        }
+    }
+
+}
---
+++ part02-Part02_28.Word/src/main/java/Word.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Word {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // String word = scanner.nextLine();
+
+
+
+    }
+
+    public static String word() {
+        return "wow";
+    }
+
+}
