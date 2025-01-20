4af7d5d6c645262345581c2e51ab54f607ce7396
Sparkimouto <155643846+HimoutoCalcifer@users.noreply.github.com>
2025-01-20 15:31:43 +0800

    Add files via upload

---
+++ part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class LargerThanOrEqualTo {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Give the first number:");
+        int one = Integer.valueOf(scan.nextLine());
+
+        System.out.println("Give the second number:");
+        int two = Integer.valueOf(scan.nextLine());
+
+        if (one == two){
+            System.out.println("The numbers are equal!");
+        } else if (one > two) {
+            System.out.println("Greater number is: " + one);
+        } else {
+            System.out.println("Greater number is: " + two);
+        }
+
+    }
+}
